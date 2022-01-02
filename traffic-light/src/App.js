cdf8c4678b6f12fa27f55751e1f8c3090434368a
minh tuan <minhtuan2001611@gail.com>
2022-01-02 17:56:12 +0700

    hoc coderx

---
+++ traffic-light/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import TrafficLight from "./components/TrafficLight.js";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <TrafficLight />
+    </div>
+  );
+}
+
+export default App;
